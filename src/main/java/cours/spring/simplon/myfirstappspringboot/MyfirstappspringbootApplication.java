247e9124317f8d75623001b9a0ed8e9db258ee75
abdoul20 <abdoulkarimfoly1996@gmail.com>
2022-05-12 16:43:16 +0000

    premiere initialisation du projet spring boot

---
+++ src/main/java/cours/spring/simplon/myfirstappspringboot/MyfirstappspringbootApplication.java
@@ -0,0 +1,13 @@
+package cours.spring.simplon.myfirstappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstappspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfirstappspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cours/spring/simplon/myfirstappspringboot/MyfirstappspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cours.spring.simplon.myfirstappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstappspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
